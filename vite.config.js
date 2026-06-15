f847cd6c2afa21fff23f1863534b6bbddbdf4a67
nishantprime <nishantkumarprime@gmail.com>
2026-06-15 23:05:35 +0530

    initial

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true // <--- This tells Vite to instantly open your browser
+  }
+})
